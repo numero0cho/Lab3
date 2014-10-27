2298d92c40a1f0e4ef4aa260fb3bc1dac062256c
Nicolas Fajardo <nicolasfajardo@email.arizona.edu>
2014-10-27 14:33:51 -0700

    Completed PWM and made it modular in another header file.

--- Lab3.c
+++ Lab3.c
@@ -34,8 +34,8 @@ IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT)
 #define F_CY 14745600
 
 // FIXME: MAY NEED TO CHANGE FREQUENCY BELOW
-#define PWM_PERIOD 1
-#define PWM_FREQ 1						// setting frequency to 100 Hz; MAY NEED TO BE CHANGED
+#define PWM_PERIOD (1/10000)
+#define PWM_FREQ 10000						// setting frequency to 100 Hz; MAY NEED TO BE CHANGED
 #define PR_VALUE (57600/PWM_FREQ)-1		// using presalar of 256 with F_CY
 
 
@@ -43,14 +43,20 @@ volatile double POT_POS = 0;
 
 int main(void) {
 
-	double OC_value_right = 0.0;
-	double OC_value_left = 0.0;
-	double DUTY_CYCLE_RIGHT = 0.0;
-	double DUTY_CYCLE_LEFT = 0.0;
+	//double OC_value_right = 0.0;
+	//double OC_value_left = 0.0;
+	//double DUTY_CYCLE_RIGHT = 0.0;
+	//double DUTY_CYCLE_LEFT = 0.0;
 
 	TRISAbits.TRISA0 = 1;		// set pin 2 to be input
 	AD1PCFGbits.PCFG0 = 0;		// set pin 2 to be analog input
 
+	TRISBbits.TRISB8 = 0;	// setting pin 17 to be output
+	TRISBbits.TRISB9 = 0;	// setting pin 18 to be output
+	RPOR4bits.RP8R = 18;	// pin RB8 (RP8) for OC1
+	RPOR4bits.RP9R = 19;	// pin RB9 (RP9) for OC2
+
+	PWM_init(POT_POS);
 	/***************************************
 	
 	TODO: Analog to digital conversions will go in here to find out the position of the
@@ -64,40 +70,11 @@ int main(void) {
 	// we have a 10-bit ADC, so the values from the approximation range from 0-1023 [2^10-1]
 	// this means that our middle value is 511.5
 
-	TRISBbits.TRISB8 = 0;	// setting pin 17 to be output
-	TRISBbits.TRISB9 = 0;	// setting pin 18 to be output
-	RPOR4bits.RP8R = 18;	// pin RB8 (RP8) for OC1
-	RPOR4bits.RP9R = 19;	// pin RB9 (RP9) for OC2
-	
-	DUTY_CYCLE_RIGHT = POT_POS / ((2 ^ 10) - 1);				// initialize the duty cycle for right motor
-	OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE / PWM_FREQ);	// initialize the output compare value for OC1
-	OC1R = 0.0;													// set the initial duty cycle to have motor OFF
-	OC1RS = OC_value_right;
-	OC1CONbits.OCTSEL = 1;
-	OC1CONbits.OCM = 6;
 
-	DUTY_CYCLE_LEFT = 1 - DUTY_CYCLE_RIGHT;						// left motor will be remaining fraction of right motor
-	OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE / PWM_FREQ);	// initialize the output compare for value for OC2
-	OC2R = 0.0;													// set the initial duty cycle to have the motor OFF
-	OC2RS = OC_value_left;
-	OC2CONbits.OCTSEL = 1;
-	OC2CONbits.OCM = 6;
-
-
-	TMR3 = 0;
-	PR3 = PR_VALUE;
-	T3CON = 0x8030;		// configures with a prescalar of 256 and turns timer 3 ON
 
 
 	while (1) {
-		DUTY_CYCLE_RIGHT = POT_POS / ((2^10) - 1);			// update duty cycle for new position
-		OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE/PWM_FREQ);	// set output compare value for new position
-		OC1RS = OC_value_right;								// set the right motor value for the next cycle
-
-		DUTY_CYCLE_LEFT = 1 - DUTY_CYCLE_RIGHT;				// update duty cycle for new position
-		OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE / PWM_FREQ);	// set output compare value for new position
-		OC2RS = OC_value_left;								// set the left mtoor value for the next cycle
-
+		PWM_Update(POT_POS);
 
 
 
--- RobotMove.c
+++ RobotMove.c
@@ -3,6 +3,69 @@
 #include "p24fj64ga002.h"
 
 // ******************************************************************************************* //
+#define F_CY 14745600
+#define PWM_PERIOD (1/10000)
+#define PWM_FREQ 10000					// setting frequency to 10 kHz
+#define PR_VALUE (57600/PWM_FREQ)-1		// using presalar of 256 with F_CY
+
+
+void PWM_init(double pot_position) {
+
+	double OC_value_right = 0.0;
+	double OC_value_left = 0.0;
+	double DUTY_CYCLE_RIGHT = 0.0;
+	double DUTY_CYCLE_LEFT = 0.0;
+
+	DUTY_CYCLE_RIGHT = pot_position / (1023 / 2);						// initialize the duty cycle for right motor
+	if (DUTY_CYCLE_RIGHT > 1.0) {
+		DUTY_CYCLE_RIGHT = 1.0;
+	}
+	OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE);				// initialize the output compare value for OC1
+	OC1R = 0.0;													// set the initial duty cycle to have motor OFF
+	OC1RS = OC_value_right;
+	OC1CONbits.OCTSEL = 1;
+	OC1CONbits.OCM = 6;
+
+	if (DUTY_CYCLE_RIGHT > 1.0)
+		DUTY_CYCLE_LEFT = 2 - DUTY_CYCLE_RIGHT;
+	else
+		DUTY_CYCLE_LEFT = 1.0;
+
+	OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE);				// initialize the output compare for value for OC2
+	OC2R = 0.0;													// set the initial duty cycle to have the motor OFF
+	OC2RS = OC_value_left;
+	OC2CONbits.OCTSEL = 1;
+	OC2CONbits.OCM = 6;
+
+
+	TMR3 = 0;
+	PR3 = PR_VALUE;
+	T3CON = 0x8030;		// configures with a prescalar of 256 and turns timer 3 ON
+
+	return;
+}
+
+void PWM_Update(double pot_position) {
+
+	double OC_value_right = 0.0;
+	double OC_value_left = 0.0;
+	double DUTY_CYCLE_RIGHT = 0.0;
+	double DUTY_CYCLE_LEFT = 0.0;
+
+	DUTY_CYCLE_RIGHT = pot_position / (1023);			// update duty cycle for new position
+	OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE);		// set output compare value for new position
+	OC1RS = OC_value_right;								// set the right motor value for the next cycle
+
+	DUTY_CYCLE_LEFT = 1 - DUTY_CYCLE_RIGHT;				// update duty cycle for new position
+	OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE);		// set output compare value for new position
+	OC2RS = OC_value_left;								// set the left mtoor value for the next cycle
+
+
+	return;
+}
+
+
+
 
 // Nicolas Fajardo, Paul Cross, Kevin Morris
 // TEAM 202
