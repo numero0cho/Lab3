c78c11b30bc87d26f880bf81bd16175d12685d73
Nicolas Fajardo <nicolasfajardo@email.arizona.edu>
2014-10-28 10:15:41 -0700

    New versions including both PWM and ADC

--- Lab3.c
+++ Lab3.c
@@ -19,26 +19,117 @@
 _CONFIG1(JTAGEN_OFF & GCP_OFF & GWRP_OFF & BKBUG_ON & COE_ON & ICS_PGx1 &
 	FWDTEN_OFF & WINDIS_OFF & FWPSA_PR128 & WDTPS_PS32768)
 
-// ******************************************************************************************* //
-// Configuration bits for CONFIG2 settings.
-// Make sure "Configuration Bits set in code." option is checked in MPLAB.
+	// ******************************************************************************************* //
+	// Configuration bits for CONFIG2 settings.
+	// Make sure "Configuration Bits set in code." option is checked in MPLAB.
 
-_CONFIG2(IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & OSCIOFNC_OFF &
+	_CONFIG2(IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & OSCIOFNC_OFF &
 	IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT)
 
-// ******************************************************************************************* //
+	// ******************************************************************************************* //
 
-// Varible used to indicate that the current configuration of the keypad has been changed,
-// and the KeypadScan() function needs to be called.
+	// Varible used to indicate that the current configuration of the keypad has been changed,
+	// and the KeypadScan() function needs to be called.
+
+#define F_CY 14745600
+
+	// FIXME: MAY NEED TO CHANGE FREQUENCY BELOW
+#define PWM_PERIOD (1/10000)
+#define PWM_FREQ 10000						// setting frequency to 100 Hz; MAY NEED TO BE CHANGED
+#define PR_VALUE (57600/PWM_FREQ)-1		// using presalar of 256 with F_CY
+
+volatile int state = 0;
+volatile double POT_POS = 0.0;
 
 int main(void) {
+	char value[8];
 
-	TRISAbits.TRISA0 = 1;
-	AD1PCFGbits.PCFG0 = 0;
+	TRISBbits.TRISB8 = 0;	// setting pin 17 to be output
+	TRISBbits.TRISB9 = 0;	// setting pin 18 to be output
+	RPOR4bits.RP8R = 18;	// pin RB8 (RP8) for OC1
+	RPOR4bits.RP9R = 19;	// pin RB9 (RP9) for OC2
+
+	TRISBbits.TRISB5 = 1;	// switch 1 is input; pin 14
+
+	CNEN2bits.CN27IE = 1;	// enable change notification for switch 1
+
+	IFS1bits.CNIF = 0;		// set CN flag low
+	IEC1bits.CNIE = 1;		// enable CN
+
+	TRISAbits.TRISA0 = 1;	// set pin 2 to be input
+	AD1PCFGbits.PCFG0 = 0;	// set pin 2 to be analog input
+
+	AD1PCFG &= 0xFFFE;	// AN0 input pin is analog
+	AD1CON2 = 0; // Configure A/D voltage reference
+	AD1CON3 = 0x0101;
+	AD1CON1 = 0x20E4;
+	AD1CHS = 0; // Configure input channel
+	AD1CSSL = 0; // No inputs is scanned
+
+	AD1CON1bits.ADON = 1; // Turn on A/D
+
+	PWM_init(POT_POS);
+
+	/***************************************
+	
+	TODO: Analog to digital conversions will go in here to find out the position of the
+	potentiometer.  This position will be used to set the duty cycle for the motion of
+	the motors in the Pulse Width Modulation (PWM).
+	
+	***************************************/
+
+	// will arbitrarily define the result of the ADC as the double pot_pos (see above)
+	
+	// we have a 10-bit ADC, so the values from the approximation range from 0-1023 [2^10-1]
+	// this means that our middle value is 511.5
+
+	while (1) {
+
+		while (AD1CON1bits.DONE != 1){};     // keeps waiting until conversion finished
+		POT_POS = ADC1BUF0;
+
+		sprintf(value, "%6d", POT_POS);
+		LCDMoveCursor(0, 0); LCDPrintString(value);
+
+		PWM_Update(POT_POS);
+
+		switch (state) {
+
+			case 0:		// Idle
+
+			case 1:		// Forward
+
+			case 2:		// Backward
+
+
+		}
+
+
+	}
+	
+
 
 
 
 
 
 	return 0;
+}
+
+
+void __attribute__((interrupt)) _CNInterrupt(void) { // for SW1 RB5
+
+	IFS1bits.CNIF = 0;
+
+	if (PORTBbits.RB5 == 1) {   // released
+		return;
+	}
+
+	//pressed (let's switch states)
+	if (state = 2) {
+		state = 0;
+	}
+	else {
+		state++;
+	}
 }
--- RobotMove.c
+++ RobotMove.c
@@ -3,6 +3,80 @@
 #include "p24fj64ga002.h"
 
 // ******************************************************************************************* //
+#define F_CY 14745600
+#define PWM_PERIOD (1/10000)
+#define PWM_FREQ 10000					// setting frequency to 10 kHz
+#define PR_VALUE (57600/PWM_FREQ)-1		// using presalar of 256 with F_CY
+
+
+void PWM_init(double pot_position) {
+
+	double OC_value_right = 0.0;
+	double OC_value_left = 0.0;
+	double DUTY_CYCLE_RIGHT = 0.0;
+	double DUTY_CYCLE_LEFT = 0.0;
+	double position_ratio = 0.0;
+
+	position_ratio = pot_position / (1023 / 2);			// update duty cycle for new position
+	if (position_ratio < 1.0) {							// turning right
+		DUTY_CYCLE_LEFT = 1.0;
+		DUTY_CYCLE_RIGHT = position_ratio;
+	}
+	else {												// else turning left or idle
+		DUTY_CYCLE_RIGHT = 1.0;							// right motor on full
+		DUTY_CYCLE_LEFT = 2 - position_ratio;			// turning left
+	}
+
+	OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE);		// set output compare value for new position
+	OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE);		// set output compare value for new position
+
+	OC1R = 0.0;													// set the initial duty cycle to have motor OFF
+	OC1RS = OC_value_right;
+	OC1CONbits.OCTSEL = 1;
+	OC1CONbits.OCM = 6;
+
+	OC2R = 0.0;													// set the initial duty cycle to have the motor OFF
+	OC2RS = OC_value_left;
+	OC2CONbits.OCTSEL = 1;
+	OC2CONbits.OCM = 6;
+
+
+	TMR3 = 0;
+	PR3 = PR_VALUE;
+	T3CON = 0x8030;		// configures with a prescalar of 256 and turns timer 3 ON
+
+	return;
+}
+
+void PWM_Update(double pot_position) {
+
+	double OC_value_right = 0.0;
+	double OC_value_left = 0.0;
+	double DUTY_CYCLE_RIGHT = 0.0;
+	double DUTY_CYCLE_LEFT = 0.0;
+	double position_ratio = 0.0;
+
+	position_ratio = pot_position / (1023 / 2);			// update duty cycle for new position
+	if (position_ratio < 1.0) {							// turning right
+		DUTY_CYCLE_LEFT = 1.0;
+		DUTY_CYCLE_RIGHT = position_ratio;
+	}
+	else {												// else turning left or idle
+		DUTY_CYCLE_RIGHT = 1.0;							// right motor on full
+		DUTY_CYCLE_LEFT = 2 - position_ratio;			// turning left
+	}
+
+	OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE);		// set output compare value for new position
+	OC1RS = OC_value_right;								// set the right motor value for the next cycle
+
+	OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE);		// set output compare value for new position
+	OC2RS = OC_value_left;								// set the left mtoor value for the next cycle
+
+	return;
+}
+
+
+
 
 // Nicolas Fajardo, Paul Cross, Kevin Morris
 // TEAM 202
