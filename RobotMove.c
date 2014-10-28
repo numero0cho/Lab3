7d63b2e23a0847711a9fc8f264877b5dfdbd9ba1
Nicolas Fajardo <nicolasfajardo@email.arizona.edu>
2014-10-27 18:21:43 -0700

    Updated PWM functions for proper functionality

--- RobotMove.c
+++ RobotMove.c
@@ -15,23 +15,26 @@ void PWM_init(double pot_position) {
 	double OC_value_left = 0.0;
 	double DUTY_CYCLE_RIGHT = 0.0;
 	double DUTY_CYCLE_LEFT = 0.0;
+	double position_ratio = 0.0;
 
-	DUTY_CYCLE_RIGHT = pot_position / (1023 / 2);						// initialize the duty cycle for right motor
-	if (DUTY_CYCLE_RIGHT > 1.0) {
-		DUTY_CYCLE_RIGHT = 1.0;
+	position_ratio = pot_position / (1023 / 2);			// update duty cycle for new position
+	if (position_ratio < 1.0) {							// turning right
+		DUTY_CYCLE_LEFT = 1.0;
+		DUTY_CYCLE_RIGHT = position_ratio;
 	}
-	OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE);				// initialize the output compare value for OC1
+	else {												// else turning left or idle
+		DUTY_CYCLE_RIGHT = 1.0;							// right motor on full
+		DUTY_CYCLE_LEFT = 2 - position_ratio;			// turning left
+	}
+
+	OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE);		// set output compare value for new position
+	OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE);		// set output compare value for new position
+
 	OC1R = 0.0;													// set the initial duty cycle to have motor OFF
 	OC1RS = OC_value_right;
 	OC1CONbits.OCTSEL = 1;
 	OC1CONbits.OCM = 6;
 
-	if (DUTY_CYCLE_RIGHT > 1.0)
-		DUTY_CYCLE_LEFT = 2 - DUTY_CYCLE_RIGHT;
-	else
-		DUTY_CYCLE_LEFT = 1.0;
-
-	OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE);				// initialize the output compare for value for OC2
 	OC2R = 0.0;													// set the initial duty cycle to have the motor OFF
 	OC2RS = OC_value_left;
 	OC2CONbits.OCTSEL = 1;
@@ -51,21 +54,23 @@ void PWM_Update(double pot_position) {
 	double OC_value_left = 0.0;
 	double DUTY_CYCLE_RIGHT = 0.0;
 	double DUTY_CYCLE_LEFT = 0.0;
+	double position_ratio = 0.0;
 
-	DUTY_CYCLE_RIGHT = pot_position / (1023 / 2);		// update duty cycle for new position
-	if (DUTY_CYCLE_RIGHT > 1.0) {
-		DUTY_CYCLE_RIGHT = 1.0;
-	}			
+	position_ratio = pot_position / (1023 / 2);			// update duty cycle for new position
+	if (position_ratio < 1.0) {							// turning right
+		DUTY_CYCLE_LEFT = 1.0;
+		DUTY_CYCLE_RIGHT = position_ratio;
+	}
+	else {												// else turning left or idle
+		DUTY_CYCLE_RIGHT = 1.0;							// right motor on full
+		DUTY_CYCLE_LEFT = 2 - position_ratio;			// turning left
+	}
+
 	OC_value_right = DUTY_CYCLE_RIGHT * (PR_VALUE);		// set output compare value for new position
 	OC1RS = OC_value_right;								// set the right motor value for the next cycle
 
-	if (DUTY_CYCLE_RIGHT > 1.0)							// update duty cycle for new position
-		DUTY_CYCLE_LEFT = 2 - DUTY_CYCLE_RIGHT;
-	else
-		DUTY_CYCLE_LEFT = 1.0;
 	OC_value_left = DUTY_CYCLE_LEFT * (PR_VALUE);		// set output compare value for new position
 	OC2RS = OC_value_left;								// set the left mtoor value for the next cycle
-
 
 	return;
 }
